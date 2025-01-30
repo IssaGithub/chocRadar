a20f5f8dd6aad60fb61b8ec438a1748500f89ed0
Issa Zaytoun <issa.zaytoun@googlemail.com>
2025-01-30 20:33:56 +0100

    feat: map link to product data

--- src/app/utility/chocolate-helper.ts
+++ src/app/utility/chocolate-helper.ts
@@ -13,10 +13,11 @@ export function transformProductData(chocolate:chocolate[]){
 
         const pricePer100g = chocolate.prices.reduce((sum,item)=>sum + item.price,0)
         const cheapestPricePer100g = Math.min(...chocolate.prices.map(price=>(price.price/price.amount)*100))
-
+        const chepeastLink = chocolate.prices.reduce((min,current) => (current.price/current.amount)*100<(min.price/min.amount)*100?current:min)
         return {...chocolate,
              "cheapestPricePer100g":cheapestPricePer100g,
-             "pricePer100g":pricePer100g
+             "pricePer100g":pricePer100g,
+             "link":chepeastLink.link
         }
     })
 }
