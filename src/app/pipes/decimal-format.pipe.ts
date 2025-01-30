7fd3fb23204a268edfc09b40642e9c2e7e5370c7
Issa Zaytoun <issa.zaytoun@googlemail.com>
2025-01-30 13:33:41 +0100

    implement pipes

---
+++ src/app/pipes/decimal-format.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'decimalFormat',
+  standalone: true
+})
+export class DecimalFormatPipe implements PipeTransform {
+
+  transform(value: number|string,decimalPlaces:number): string {
+    if(value===null || isNaN(Number(value))){
+      return '';
+    }
+
+    return Number(value).toFixed(decimalPlaces)
+  }
+
+}
