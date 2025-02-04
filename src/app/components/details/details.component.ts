642407adb2a7947578c6fe3aadf0984616464ac2
issa <issa.zaytoun@googlemail.com>
2025-02-04 21:19:37 +0100

    feature: edit brand and name

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
@@ -6,14 +6,33 @@ import { cheapestPrice } from '../../utility/product-helper';
 import { ProductService } from '../../services/http/chocolate.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'choc-details',
   standalone: true,
-  imports: [MatTableModule, CommonModule, FontAwesomeModule],
+  imports: [
+    MatTableModule,
+    MatFormFieldModule,
+    CommonModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
   brand = this.dataService.selectedProduct().brand;
   name = this.dataService.selectedProduct().name;
   prices = this.dataService.selectedProduct().prices;
@@ -21,16 +40,29 @@ export class DetailsComponent {
   productId?: string | null;
   displayedColumns: string[] = ['price', 'cheapest', 'amount', 'link'];
   faEdit = faEdit;
+  editMode = false;
 
+  titleGroup = new FormGroup({
+    brand: new FormControl(this.brand),
+    name: new FormControl(this.name),
+  });
   cheapestPrice = cheapestPrice(this.prices);
-  constructor(
-    private route: ActivatedRoute,
-    private dataService: ProductService,
-  ) {}
+  constructor(private dataService: ProductService) {}
 
-  ngOnnit() {
-    this.route.queryParamMap.subscribe((paramMap) => {
-      this.productId = paramMap.get('id');
+  editTitle() {
+    this.editMode = true;
+  }
+  submitForm() {
+    this.editMode = !this.editMode;
+  }
+
+  ngOnInit() {
+    this.titleGroup.get('brand')!.valueChanges.subscribe((value) => {
+      if (value) this.brand = value;
+    });
+
+    this.titleGroup.get('name')!.valueChanges.subscribe((value) => {
+      if (value) this.name = value;
     });
   }
 }
