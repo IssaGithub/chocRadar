0ade4184a8230cc63e2aae0f6067fd073f4de708
issa <issa.zaytoun@googlemail.com>
2025-02-04 13:31:57 +0100

    feature: add edit button

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -4,23 +4,29 @@ import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { cheapestPrice } from '../../utility/product-helper';
 import { ProductService } from '../../services/http/chocolate.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'choc-details',
   standalone: true,
-  imports: [MatTableModule, CommonModule],
+  imports: [MatTableModule, CommonModule, FontAwesomeModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
 export class DetailsComponent {
   brand = this.dataService.selectedProduct().brand;
   name = this.dataService.selectedProduct().name;
- prices= this.dataService.selectedProduct().prices;
- 
+  prices = this.dataService.selectedProduct().prices;
+
   productId?: string | null;
   displayedColumns: string[] = ['price', 'cheapest', 'amount', 'link'];
+  faEdit = faEdit;
 
   cheapestPrice = cheapestPrice(this.prices);
-  constructor(private route: ActivatedRoute,private dataService:ProductService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: ProductService,
+  ) {}
 
   ngOnnit() {
     this.route.queryParamMap.subscribe((paramMap) => {
